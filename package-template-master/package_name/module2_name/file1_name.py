77c59b23b29ac0ba5ea44120e7cfd04b87a6ff65
gracielle-ch <59579212+gracielle-ch@users.noreply.github.com>
2022-11-17 15:49:28 -0300

    Create file1_name.py

---
+++ package-template-master/package_name/module2_name/file1_name.py
@@ -0,0 +1,6 @@
+'''pip install -i https://test.pypi.org/simple/ package-template-master==0.0.2
+Looking in indexes: https://test.pypi.org/simple/
+Collecting package-template-master==0.0.2
+  Downloading https://test-files.pythonhosted.org/packages/b6/9f/4eb027c36ba4a03e50b0a6e4068f5eb12aea61e4e0148b954f75e3fb31c5/package_template_master-0.0.2-py3-none-any.whl (2.6 kB)       
+Installing collected packages: package-template-master
+Successfully installed package-template-master-0.0.2'''
